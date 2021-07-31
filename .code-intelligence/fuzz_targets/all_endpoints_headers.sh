7b7583590d5debee4c2ca611f52a41220eae930b
wojtek <dziewiecki@code-intelligence.com>
2021-07-31 11:36:54 +0200

    working header script, thus working authentication to WebGoat

--- .code-intelligence/fuzz_targets/all_endpoints_headers.sh
+++ .code-intelligence/fuzz_targets/all_endpoints_headers.sh
@@ -1,6 +1,7 @@
-USER="user$RANDOM"
-PASS="pass$RANDOM"
-curl --max-redirs 0 -X POST 'http://172.17.0.4:8080/WebGoat/register.mvc' -X POST -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'Content-Type: application/x-www-form-urlencoded' -H 'Origin: http://172.17.0.4:8080' -H 'Connection: keep-alive' -H 'Referer: http://172.17.0.4:8080/WebGoat/register.mvc' -H 'Upgrade-Insecure-Requests: 1' -H 'Sec-GPC: 1' --data-raw "username=$USER&password=$PASS&matchingPassword=$PASS&agree=agree" -c dupa
-
-cat dupa
-#curl 'http://172.17.0.4:8080/WebGoat/register.mvc' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'Content-Type: application/x-www-form-urlencoded' -H 'Origin: http://172.17.0.4:8080' -H 'Connection: keep-alive' -H 'Referer: http://172.17.0.4:8080/WebGoat/register.mvc' -H 'Upgrade-Insecure-Requests: 1' -H 'Sec-GPC: 1' --data-raw "username=$USER&password=$PASS&matchingPassword=$PASS&agree=agree" -w |grep SESSIONID |cut -d ' ' -f 2
+WGUSER=cifuzz
+WGPASS=nnMjJa72lx
+WGCOOK="/tmp/webgoatcookies$WGUSER"
+curl -s -X 'POST' 'http://172.17.0.3:8080/WebGoat/register.mvc' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'Content-Type: application/x-www-form-urlencoded' -H 'Origin: http://172.17.0.3:8080' -H 'Connection: keep-alive' -H 'Referer: http://172.17.0.3:8080/WebGoat/register.mvc' -H 'Upgrade-Insecure-Requests: 1' -H 'Sec-GPC: 1' --data-raw "username=$USER&password=$WGPASS&matchingPassword=$WGPASS&agree=agree" -c $WGCOOK >/dev/null
+curl -s 'http://172.17.0.3:8080/WebGoat/login' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'Content-Type: application/x-www-form-urlencoded' -H 'Origin: http://172.17.0.3:8080' -H 'Connection: keep-alive' -H 'Referer: http://172.17.0.3:8080/WebGoat/login?error' -H 'Upgrade-Insecure-Requests: 1' -H 'Sec-GPC: 1' --data-raw "username=$USER&password=$WGPASS&matchingPassword=$WGPASS&agree=agree" -c $WGCOOK >/dev/null
+echo -n Cookie: JSESSIONID= && cat $WGCOOK|grep JSESSIONID|cut -f 7
+rm $WGCOOK
