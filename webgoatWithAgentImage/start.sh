795a8c2eb25b91d086bd2f1b141834989a03b139
wojtek <dziewiecki@code-intelligence.com>
2021-09-27 13:05:59 +0200

    Add a CICD script with a deployment to Azure and fuzzing, modified fuzz test to target the Azure deployment, add Java agent

--- webgoatWithAgentImage/start.sh
+++ webgoatWithAgentImage/start.sh
@@ -5,7 +5,7 @@ service nginx start
 sleep 1
 echo "Starting WebGoat..."
 java -Duser.home=/home/webgoat -Dfile.encoding=UTF-8 \
--javaagent:/cibin/fuzzing_agent_deploy.jar=instrumentation_includes="org.owasp.webgoat.**",fuzzing_server_host=172.17.0.1,service_name=projects/organizations_89d295318b3fded6_webgoat_git-95e1f92d/web_services/webgoat \
+-javaagent:/cibin/fuzzing_agent_deploy.jar=tls=true,api_token=CI_FUZZ_API_TOKEN,instrumentation_includes="org.owasp.webgoat.**",fuzzing_server_host=172.17.0.1,service_name=projects/organizations_89d295318b3fded6_webgoat_git-95e1f92d/web_services/webgoat \
 -jar webgoat.jar --webgoat.build.version=$1 --server.address=0.0.0.0 > webgoat.log &
 
 sleep 10
